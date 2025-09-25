f03c4f9d57ef5021dcb19121c8993cfcb8cb5d78
f-wc <354002894@qq.com>
2025-09-25 23:25:51 +0800

    process HMDB structures.zip file (SDF format)

---
+++ src/dGbyG/data/hmdb_data.py
@@ -0,0 +1,33 @@
+import os
+import zipfile
+import pandas as pd
+from io import BytesIO
+from rdkit import Chem
+
+from ..utils.config import config
+
+def raw_file_to_csv(zip_file='structures_version5.0.zip'):
+    """
+    # raw data was downloaded from https://hmdb.ca/downloads
+    """
+    zip_path = os.path.join(config.hmdb_database_path, zip_file)
+    print(f"Extracting {zip_path}")
+    with zipfile.ZipFile(zip_path, 'r') as zf:
+        sdf_files = [f for f in zf.namelist() if f.lower().endswith('.sdf')]
+        for file in sdf_files:
+            with zf.open(file) as f:
+                f = BytesIO(f.read())
+                supplier = Chem.ForwardSDMolSupplier(f)
+    
+    data = []
+    for mol in supplier:
+        if mol is not None:
+            data.append([mol.GetProp('HMDB_ID'), mol.GetProp('SMILES')])
+    df = pd.DataFrame(data, columns=['HMDB_ID', 'SMILES'])
+    
+    csv_path = os.path.join(config.hmdb_database_path, 'structures.csv')
+    print(f"Writing {csv_path}")
+    with open(csv_path, 'w', encoding='utf-8') as f:
+        f.write(f"# {zip_file}\n")
+        df.to_csv(f, mode='a', index=False, header=True)
+    print(f"Done.")
