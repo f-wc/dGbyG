9d9dcbe7306f020e0f15fe76f40e5f0bb6c2777b
f-wc <354002894@qq.com>
2025-10-09 22:44:08 +0800

    compress structures file

--- src/dGbyG/data/hmdb_data.py
+++ src/dGbyG/data/hmdb_data.py
@@ -1,12 +1,12 @@
 import os
 import zipfile
 import pandas as pd
-from io import BytesIO
+from io import BytesIO, StringIO
 from rdkit import Chem
 
 from ..utils.config import config
 
-def raw_file_to_csv(zip_file='structures_version5.0.zip'):
+def raw_file_to_csv(zip_file='structures.zip'):
     """
     # raw data was downloaded from https://hmdb.ca/downloads
     """
@@ -19,15 +19,19 @@ def raw_file_to_csv(zip_file='structures_version5.0.zip'):
                 f = BytesIO(f.read())
                 supplier = Chem.ForwardSDMolSupplier(f)
     
+    csv_buffer = StringIO()
+    csv_buffer.write(f"# {zip_file}\n")
+
     data = []
     for mol in supplier:
         if mol is not None:
             data.append([mol.GetProp('HMDB_ID'), mol.GetProp('SMILES')])
     df = pd.DataFrame(data, columns=['HMDB_ID', 'SMILES'])
+    df.to_csv(csv_buffer, mode='a', index=False, header=True)
     
-    csv_path = os.path.join(config.hmdb_database_path, 'structures.csv')
+    # write to file
+    csv_path = os.path.join(config.hmdb_database_path, 'structures.csv.zip')
     print(f"Writing {csv_path}")
-    with open(csv_path, 'w', encoding='utf-8') as f:
-        f.write(f"# {zip_file}\n")
-        df.to_csv(f, mode='a', index=False, header=True)
+    with zipfile.ZipFile(csv_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipf.writestr(csv_path, csv_buffer.getvalue())
     print(f"Done.")
