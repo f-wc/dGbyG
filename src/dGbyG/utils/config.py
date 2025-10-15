be4332a58d271bc91efc7de6c8b7ee8e2d7e91e8
f-wc <354002894@qq.com>
2025-10-15 19:24:49 +0800

    rename: rename libchebi to chebi

--- src/dGbyG/utils/config.py
+++ src/dGbyG/utils/config.py
@@ -7,7 +7,7 @@ class Config:
         self.kegg_database_path = os.path.join(__file__.split('src')[0], 'data', 'KEGG')
         self.hmdb_database_path = os.path.join(__file__.split('src')[0], 'data', 'HMDB')
         self.bigg_database_path = os.path.join(__file__.split('src')[0], 'data', 'BiGG')
-        self.chebi_database_path = os.path.join(__file__.split('src')[0], 'data', 'libChEBI')
+        self.chebi_database_path = os.path.join(__file__.split('src')[0], 'data', 'ChEBI')
         self.lipidmaps_database_path = os.path.join(__file__.split('src')[0], 'data', 'LIPID_MAPS')
         self.pubchem_database_path = os.path.join(__file__.split('src')[0], 'data', 'PubChem')
         self.metanetx_database_path = os.path.join(__file__.split('src')[0], 'data', 'MetaNetX')
